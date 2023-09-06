7fb94585a24eb697eda9a28937d0294d969efcf8
Adolfo012 <adolfo.lamas5156@alumnos.udg.mx>
2023-09-06 16:45:48 -0600

    Clase miercoles 6 de septiembre

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -8,19 +8,36 @@
 </head>
 <body>
     <h1>Formulario de Contacto</h1>
-    <form action="" method="POST">
+    <form action="contacto" method="POST">
+
+        <!-- <?php echo $tipo; ?> -->
+        <h3>{{ $tipo }} </h3>
+
+        @csrf
         <label for="correo">Correo:</label><br>
-        <input type="email" name="correo"><br>
+        <!-- <input type="email" name="correo" value="@alumnos.udg.mx"><br> -->
+        <input type="email" name="correo"
+        placeholder="example@gmail.com"
+        @if ($tipo == "alumno")
+
+            value="@alumnos.udg.mx"
+
+        @else ($tipo == "empleado")
+
+            value="@empleado.udg.mx"
+
+        @endif 
+        ><br>
         <label for="comentario">Comentario:</label><br>
         <textarea name="comentario" id="comentario" cols="30" rows="10">
 
         </textarea>
         <br>
-        <label for="tipo">Tipo:</label><br>
+        <!-- <label for="tipo">Tipo:</label><br>
         <select name="tipo" id="">
             <option value="Alumno">Alumno</option>
             <option value="Empleado">Empleado</option>
-        </select>
+        </select> -->
         <input type="submit" name="enviar">
     </form>
 </body>
